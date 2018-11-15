5a01950c14b058ce631b2b91bafc743a21622ac5
AndrewChen1982 <39516588+AndrewChen1982@users.noreply.github.com>
2018-11-15 14:00:44 +0800

    Remove useless code
    Remove useless code

--- Source/ServerConfig.js
+++ Source/ServerConfig.js
@@ -1,12 +1,6 @@
 "use strict";
 
-const DBAddress = "localhost";
-const DB_PWD = "your db password";
-const DB_USER = "your db user";
 
 const port = 80; // any valid port you like
 
-exports.DBAddress = DBAddress;
-exports.DB_PWD = DB_PWD;
-exports.DB_USER = DB_USER;
 exports.PORT = port;
